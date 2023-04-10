0f19f9aff820842311d5090bed339f9b247d6eed
luka <lmach19@freeuni.edu.ge>
2023-04-10 11:53:21 +0400

    Some test functions added

---
+++ app/core/admin/admin.py
@@ -0,0 +1,8 @@
+from dataclasses import dataclass
+from enum import Enum
+from typing import List, Protocol
+
+from result import Err, Ok, Result
+
+
+ADMIN_KEY = "sezam-gaighe"
--- app/infra/inmemory/student.py
+++ app/infra/inmemory/student.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass, field
-from typing import Optional
+from typing import Optional, Dict
+
 
 @dataclass
 class Student:
