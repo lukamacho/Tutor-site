d543fe4f08a88f9e78b2a0c4224642f2b05a7fa9
Shorena <sjanj19@freeuni.edu.ge>
2023-04-26 23:27:38 +0400

    Updated course interactor

--- app/core/course/interactor.py
+++ app/core/course/interactor.py
@@ -30,13 +30,11 @@ class ICourseInteractor(Protocol):
 class CourseInteractor:
     course_repository: ICourseRepository
 
-    def get_course(
-        self,
-    ) -> None:
-        self.course_repository.get_course()
+    def create_course(self, subject: str, tutor_mail: str, price: int) -> Course:
+        return self.course_repository.create_course(subject, tutor_mail, price)
 
-    def create_course(self):
-        self.course_repository.create_course()
+    def get_course(self, subject: str, tutor_mail: str) -> Optional[Course]:
+        return self.course_repository.get_course(subject, tutor_mail)
 
-    def get_courses(self):
-        self.course_repository.get_courses()
+    def get_courses(self, tutor_mail: str) -> Optional[List[Course]]:
+        return self.course_repository.get_courses(tutor_mail)
