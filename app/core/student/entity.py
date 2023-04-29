05b4ac60a3132fe07a5c070dcce4e763c62b6b63
Shorena <sjanj19@freeuni.edu.ge>
2023-04-29 11:05:01 +0400

    Separated student entity class and in memory repository

--- app/core/student/entity.py
+++ app/core/student/entity.py
@@ -1,4 +1,5 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from typing import Dict
 
 
 @dataclass
@@ -7,5 +8,42 @@ class Student:
     last_name: str
     email: str
     password: str
-    balance: int
-    profile_address: str
+    balance: 0
+    profile_address: str = ""
+    lessons: Dict[int, int] = field(default_factory=dict)
+
+    def set_balance(self, new_balance: int) -> None:
+        self.balance = new_balance
+
+    def get_balance(self) -> int:
+        return self.balance
+
+    def increase_balance(self, amount: int) -> None:
+        self.balance += amount
+
+    def decrease_balance(self, amount: int) -> None:
+        self.balance -= amount
+
+    def change_first_name(self, first_name: str) -> None:
+        self.first_name = first_name
+
+    def change_last_name(self, last_name: str) -> None:
+        self.last_name = last_name
+
+    # def set_student_info(self, first_name: str, last_name: str) -> None:
+    #     self.first_name = first_name
+    #     self.last_name = last_name
+    #
+    # def add_lessons(self, course_id: int, num_lessons: int) -> None:
+    #     self.lessons[course_id] += num_lessons
+    #
+    # def book_lesson(self, course_id: int, timeslot: str) -> None:
+    #     pass
+    #
+    # def buy_lessons(self, course_id: int, num_lessons: int) -> None:
+    #     pass
+    #
+    # def reschedule_lesson(
+    #     self, course_id: int, old_timeslot: str, new_timeslot: str
+    # ) -> None:
+    #     pass
--- app/infra/inmemory/student.py
+++ app/infra/inmemory/student.py
@@ -1,51 +1,7 @@
 from dataclasses import dataclass, field
 from typing import Dict, Optional
+from app.core.student.entity import Student
 
-
-@dataclass
-class Student:
-    first_name: str
-    last_name: str
-    email: str
-    password: str
-    balance: 0
-    lessons: Dict[int, int] = field(default_factory=dict)
-
-    def set_balance(self, new_balance: int) -> None:
-        self.balance = new_balance
-
-    def get_balance(self) -> int:
-        return self.balance
-
-    def increase_balance(self, amount: int) -> None:
-        self.balance += amount
-
-    def decrease_balance(self, amount: int) -> None:
-        self.balance -= amount
-
-    def change_first_name(self, first_name: str) -> None:
-        self.first_name = first_name
-
-    def change_last_name(self, last_name: str) -> None:
-        self.last_name = last_name
-
-    # def set_student_info(self, first_name: str, last_name: str) -> None:
-    #     self.first_name = first_name
-    #     self.last_name = last_name
-    #
-    # def add_lessons(self, course_id: int, num_lessons: int) -> None:
-    #     self.lessons[course_id] += num_lessons
-    #
-    # def book_lesson(self, course_id: int, timeslot: str) -> None:
-    #     pass
-    #
-    # def buy_lessons(self, course_id: int, num_lessons: int) -> None:
-    #     pass
-    #
-    # def reschedule_lesson(
-    #     self, course_id: int, old_timeslot: str, new_timeslot: str
-    # ) -> None:
-    #     pass
 
 @dataclass
 class InMemoryStudentRepository:
