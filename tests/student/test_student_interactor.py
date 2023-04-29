3ed38ba2826b9bb7d2429af2ca66e297ed773d1e
Shorena <sjanj19@freeuni.edu.ge>
2023-04-29 11:38:39 +0400

    Implemented tests for student interactor

--- tests/student/test_student_interactor.py
+++ tests/student/test_student_interactor.py
@@ -1,10 +1,23 @@
-from app.core.student.interactor import StudentInteractor
+from app.core.student.interactor import StudentInteractor, IStudentRepository
+from app.core.student.entity import Student
 
-def test_create_student() -> None:
-    interactor = StudentInteractor()
-    email = "daenerys@gmail.com"
-    password = "dragons"
 
-    response = interactor.create_student(email, password)
+def test_create_student(student_repository: IStudentRepository) -> None:
+    interactor = StudentInteractor(student_repository)
 
-    assert response is None
+    response = interactor.create_student("John", "Doe", "johndoe@gmail.com", "johndoe", 50)
+
+    assert isinstance(response, Student)
+    assert student_repository.get_student("johndoe@gmail.com") is not None
+    assert student_repository.get_student("") is None
+
+
+def test_create_students(student_repository: IStudentRepository) -> None:
+    interactor = StudentInteractor(student_repository)
+
+    for i in range(10000):
+        mail = "johndoe{i}@gmail.com".format(i=i)
+        response = interactor.create_student("John", "Doe", mail, "johndoe", 0)
+        assert isinstance(response, Student)
+        assert student_repository.get_student(mail) is not None
+
