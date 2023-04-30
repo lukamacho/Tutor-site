12f88bb49dae87c01e80013d9a496348f33ce6ca
Shorena <sjanj19@freeuni.edu.ge>
2023-05-01 00:00:11 +0400

    Implemented in memory tutor repository

--- app/infra/inmemory/tutor.py
+++ app/infra/inmemory/tutor.py
@@ -1,26 +1,67 @@
 from dataclasses import dataclass, field
-from typing import Dict, List, Optional
+from typing import Dict, Optional
+
+from app.core.tutor.entity import Tutor
 
 
 @dataclass
-class Tutor:
-    is_top_tutor = False
-    first_name: str
-    last_name: str
-    languages: List[str]
-    biography: str
-    current_balance = 0
-    commission_pct = 0.25
-    subject_prices: Dict[str, int] = field(default_factory=dict)
-    availability_calendar: Dict[str, int] = field(default_factory=dict)
+class InMemoryTutorRepository:
+    data: Dict[str, Tutor] = field(default_factory=dict)
 
-    def set_tutor_info(self, first_name: str, last_name: str, biography: str) -> None:
-        self.first_name = first_name
-        self.last_name = last_name
-        self.biography = biography
+    def create_tutor(
+            self, first_name: str, last_name: str, email: str, password: str, balance: int, biography: str
+    ) -> Tutor:
+        tutor = Tutor(first_name, last_name, email, password, 0.25, balance, biography)
+        self.data[email] = tutor
+        return tutor
 
-    def add_homework(self, student_id: int, homework_text: str) -> None:
-        pass
+    def get_tutor(self, email: str) -> Optional[Tutor]:
+        if email in self.data.keys():
+            return self.data[email]
+        return None
 
-    def add_language(self, language: str) -> None:
-        self.languages.append(language)
+    def set_balance(self, tutor_mail: str, new_balance: int) -> None:
+        tutor = self.get_tutor(tutor_mail)
+        if tutor is not None:
+            tutor.set_balance(new_balance)
+
+    def get_balance(self, student_mail: str) -> int:
+        tutor = self.get_tutor(student_mail)
+        if tutor is not None:
+            return tutor.get_balance()
+        return 0
+
+    def increase_balance(self, tutor_mail: str, amount: int) -> None:
+        tutor = self.get_tutor(tutor_mail)
+        if tutor is not None:
+            return tutor.increase_balance(amount)
+
+    def decrease_balance(self, tutor_mail: str, amount: int) -> None:
+        tutor = self.get_tutor(tutor_mail)
+        if tutor is not None:
+            return tutor.decrease_balance(amount)
+
+    def set_commission_pct(self, tutor_mail: str, new_commission_pct: float):
+        tutor = self.get_tutor(tutor_mail)
+        if tutor is not None:
+            return tutor.set_commission_pct(new_commission_pct)
+
+    def decrease_commission_pct(self, tutor_mail: str) -> None:
+        tutor = self.get_tutor(tutor_mail)
+        if tutor is not None:
+            return tutor.decrease_commission_pct()
+
+    def change_first_name(self, tutor_mail: str, first_name: str) -> None:
+        tutor = self.get_tutor(tutor_mail)
+        if tutor is not None:
+            tutor.change_first_name(first_name)
+
+    def change_last_name(self, tutor_mail: str, last_name: str) -> None:
+        tutor = self.get_tutor(tutor_mail)
+        if tutor is not None:
+            tutor.change_last_name(last_name)
+
+    def change_biography(self, tutor_mail: str, biography: str) -> None:
+        tutor = self.get_tutor(tutor_mail)
+        if tutor is not None:
+            tutor.change_biography(biography)
