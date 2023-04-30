ee94ab80dbf7ca4d3dee235c1ccaf6a0e168f602
Shorena <sjanj19@freeuni.edu.ge>
2023-05-01 00:54:01 +0400

    Added course repository fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,13 @@
 import pytest
 from _pytest.config.argparsing import Parser
 
+from app.core.course.interactor import ICourseRepository
 from app.core.student.interactor import IStudentRepository
 from app.core.tutor.interactor import ITutorRepository
+from app.infra.inmemory.course import InMemoryCourseRepository
 from app.infra.inmemory.student import InMemoryStudentRepository
 from app.infra.inmemory.tutor import InMemoryTutorRepository
+from app.infra.sqlite.course import SqlCourseRepository
 from app.infra.sqlite.student import SqlStudentRepository
 from app.infra.sqlite.tutors import SqlTutorRepository
 
@@ -34,3 +37,10 @@ def tutor_repository(request: pytest.FixtureRequest) -> ITutorRepository:
     if use_sql(request):
         return SqlTutorRepository("")
     return InMemoryTutorRepository()
+
+
+@pytest.fixture(scope="function")
+def course_repository(request: pytest.FixtureRequest) -> ICourseRepository:
+    if use_sql(request):
+        return SqlCourseRepository("")
+    return InMemoryCourseRepository()
