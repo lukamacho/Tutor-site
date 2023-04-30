99941192f3a57840d7204221c19f9847adb36cb3
Shorena <sjanj19@freeuni.edu.ge>
2023-05-01 00:53:50 +0400

    Updated course and student classes

--- app/core/course/entity.py
+++ app/core/course/entity.py
@@ -6,3 +6,8 @@ class Course:
     subject: str
     tutor_mail: str
     price: int
+
+    def set_course_info(self, subject: str, tutor_mail: str, price: int) -> None:
+        self.subject = subject
+        self.tutor_mail = tutor_mail
+        self.price = price
--- app/core/student/entity.py
+++ app/core/student/entity.py
@@ -30,20 +30,20 @@ class Student:
     def change_last_name(self, last_name: str) -> None:
         self.last_name = last_name
 
-    # def set_student_info(self, first_name: str, last_name: str) -> None:
-    #     self.first_name = first_name
-    #     self.last_name = last_name
-    #
-    # def add_lessons(self, course_id: int, num_lessons: int) -> None:
-    #     self.lessons[course_id] += num_lessons
-    #
-    # def book_lesson(self, course_id: int, timeslot: str) -> None:
-    #     pass
-    #
-    # def buy_lessons(self, course_id: int, num_lessons: int) -> None:
-    #     pass
-    #
-    # def reschedule_lesson(
-    #     self, course_id: int, old_timeslot: str, new_timeslot: str
-    # ) -> None:
-    #     pass
+    def set_student_info(self, first_name: str, last_name: str) -> None:
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def add_lessons(self, course_id: int, num_lessons: int) -> None:
+        self.lessons[course_id] += num_lessons
+
+    def book_lesson(self, course_id: int, timeslot: str) -> None:
+        pass
+
+    def buy_lessons(self, course_id: int, num_lessons: int) -> None:
+        pass
+
+    def reschedule_lesson(
+        self, course_id: int, old_timeslot: str, new_timeslot: str
+    ) -> None:
+        pass
