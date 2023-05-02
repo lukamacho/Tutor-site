57bb21f62a2fd41101b40547cf513332a0c1eef6
luka <lmach19@freeuni.edu.ge>
2023-05-02 11:51:56 +0400

    Student api

--- app/infra/fastapi/student_api.py
+++ app/infra/fastapi/student_api.py
@@ -1,8 +1,17 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+
+from app.core.facade import OlympianTutorService
+from app.infra.fastapi.dependables import get_core
 
 student_api = APIRouter()
 
 
-@student_api.get("/student/{student_id}")
-def get_student(student_id: int):
+@student_api.get("/student/{student_mail}")
+def get_student(
+    student_mail: str,
+    core: OlympianTutorService = Depends(get_core),
+):
+    student = core.get_student(student_mail)
+    core.change_student_last_name(student_mail, student.last_name)
+    core.change_student_first_name(student_mail, student.first_name)
     pass
