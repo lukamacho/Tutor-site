3f094402b4dbe061dc1b018a05a669814e31830a
Shorena <sjanj19@freeuni.edu.ge>
2023-05-03 09:39:06 +0400

    Implemented in memory review repository

---
+++ app/infra/inmemory/review.py
@@ -0,0 +1,41 @@
+from dataclasses import dataclass, field
+from typing import List, Optional
+
+from app.core.review.entity import Review
+
+
+@dataclass
+class InMemoryReviewRepository:
+    data: List[Review] = field(default_factory=list)
+
+    def create_review(
+        self, review_text: str, tutor_mail: str, student_mail: str
+    ) -> Review:
+        review = Review(review_text, tutor_mail, student_mail)
+        self.data.append(review)
+        return review
+
+    def get_review(self, tutor_mail: str, student_mail: str) -> Optional[Review]:
+        for review in self.data:
+            if review.tutor_mail == tutor_mail and review.student_mail == student_mail:
+                return review
+        return None
+
+    def get_tutor_reviews(self, tutor_mail: str) -> List[Review]:
+        reviews: List[Review] = []
+        for review in self.data:
+            if review.tutor_mail == tutor_mail:
+                reviews.append(review)
+        return reviews
+
+    def delete_review(self, tutor_mail: str, student_mail: str) -> None:
+        for review in self.data:
+            if review.tutor_mail == tutor_mail and review.student_mail == student_mail:
+                self.data.remove(review)
+
+    def change_review(
+        self, new_review_text: str, tutor_mail: str, student_mail: str
+    ) -> None:
+        for review in self.data:
+            if review.tutor_mail == tutor_mail and review.student_mail == student_mail:
+                review.review_text = new_review_text
