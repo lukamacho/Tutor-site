9fc98c8f01d7cf2a0a8eb52054acf6b96ed33897
Shorena <sjanj19@freeuni.edu.ge>
2023-05-03 09:40:08 +0400

    Added review repository to conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,16 +4,19 @@ from _pytest.config.argparsing import Parser
 from app.core.course.interactor import ICourseRepository
 from app.core.homework.interactor import IHomeworkRepository
 from app.core.lesson.interactor import ILessonRepository
+from app.core.review.interactor import IReviewRepository
 from app.core.student.interactor import IStudentRepository
 from app.core.tutor.interactor import ITutorRepository
 from app.infra.inmemory.course import InMemoryCourseRepository
 from app.infra.inmemory.homework import InMemoryHomeworkRepository
 from app.infra.inmemory.lesson import InMemoryLessonRepository
+from app.infra.inmemory.review import InMemoryReviewRepository
 from app.infra.inmemory.student import InMemoryStudentRepository
 from app.infra.inmemory.tutor import InMemoryTutorRepository
 from app.infra.sqlite.course import SqlCourseRepository
 from app.infra.sqlite.homework import SqlHomeworkRepository
 from app.infra.sqlite.lesson import SqlLessonRepository
+from app.infra.sqlite.review import SqlReviewRepository
 from app.infra.sqlite.student import SqlStudentRepository
 from app.infra.sqlite.tutors import SqlTutorRepository
 
@@ -64,3 +67,10 @@ def lesson_repository(request: pytest.FixtureRequest) -> ILessonRepository:
     if use_sql(request):
         return SqlLessonRepository("")
     return InMemoryLessonRepository()
+
+
+@pytest.fixture(scope="function")
+def review_repository(request: pytest.FixtureRequest) -> IReviewRepository:
+    if use_sql(request):
+        return SqlReviewRepository("")
+    return InMemoryReviewRepository()
