f35d9c94a8df73067dd730767870006d7d48baae
luka <lmach19@freeuni.edu.ge>
2023-05-28 15:04:27 +0400

    Admin part changed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,19 +3,21 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Homepage from "./Pages/Homepage"
 import Registration from "./Pages/Registration"
 import Login from "./Pages/Login"
+import Admin from "./Pages/Admin"
 
 export default function App() {
     return (
         <BrowserRouter>
-            <ul>
-                <li><Link to="/">Homepage</Link></li>
-                <li><Link to="/registration">Registration</Link></li>
-                <li><Link to="/login">Login</Link></li>
-            </ul>
+{/*             <ul> */}
+{/*                 <li><Link to="/">Homepage</Link></li> */}
+{/*                 <li><Link to="/registration">Registration</Link></li> */}
+{/*                 <li><Link to="/login">Login</Link></li> */}
+{/*             </ul> */}
             <Routes>
                 <Route path="/" element={<Homepage />} />
                 <Route path="registration" element={<Registration />} />
                 <Route path="login" element={<Login />} />
+                <Route path="admin" element={<Admin />}/>
             </Routes>
         </BrowserRouter>
     );
