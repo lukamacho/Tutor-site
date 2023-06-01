626ca294f958591974b6f79993021de9bd32e12b
luka <lmach19@freeuni.edu.ge>
2023-06-01 09:58:14 +0400

    Login front parts added

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -1,5 +1,189 @@
-export default function Login() {
-    return (
-        <h2>Login</h2>
-    );
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { useState } from 'react';
+import { useNavigate} from 'react-router-dom';
+
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Tutor site
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const defaultTheme = createTheme();
+
+export default function SignIn() {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (e, is_student)=> {
+    e.preventDefault();
+
+    const data = {
+      "user_mail": email,
+      "password": password,
+      "is_student": is_student,
+    };
+
+    try {
+      const response = await fetch('http://localhost:8000/sign_in', {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: { 'Content-Type': 'application/json' },
+      });
+      const result = await response.json();
+      console.log(result);
+
+      if (response.ok) {
+        if (is_student){
+            navigate('/student_profile', { state: { email } });
+        } else{
+            navigate('/tutor_profile', { state: { email } });
+        }
+      } else {
+        // Handle sign-in failure
+        // ...
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleResetPassword = async (e, is_student) => {
+    e.preventDefault();
+
+    const data = {
+      "user_mail": email,
+      "is_student": is_student,
+    };
+
+    try {
+      const response = await fetch('http://localhost:8000/admin/reset_password', {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: { 'Content-Type': 'application/json' },
+      });
+      const result = await response.json();
+      console.log(result);
+
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box  noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+              onClick={(e) => handleSubmit(e, true)}
+            >
+              Student Sign In
+            </Button>
+            <Button
+               type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+              onClick={(e) => handleSubmit(e, false)}
+            >
+              Tutor Sign In
+            </Button>
+            <div>
+                <Button
+                    onClick={(e) => handleResetPassword(e, false)}
+                    sx={{
+                        margin: 1,
+                    }}
+                    variant="outlined"
+                >
+                    Tutor forgot password
+                </Button>
+                <Button
+                    onClick={(e) => handleResetPassword(e, true)}
+                    sx={{
+                        margin: 1,
+                    }}
+                    variant="outlined"
+                >
+                    Student forgot password
+                </Button>
+            </div>
+              <Grid item>
+                <Link href="/registration" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 8, mb: 4 }} />
+      </Container>
+    </ThemeProvider>
+  );
 }
