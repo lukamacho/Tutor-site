4d1175dadb0a59123b29910308c3729eb3261470
Shorena <sjanj19@freeuni.edu.ge>
2023-06-08 19:18:56 +0400

    Added an initial version of the student page
    - Student can upload a profile image
    - Student can view their personal information
    - Student can change first name, last name, password, profile picture

--- app/infra/fastapi/student_api.py
+++ app/infra/fastapi/student_api.py
@@ -1,17 +1,107 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, File, UploadFile
+from pydantic import BaseModel
 
 from app.core.facade import OlympianTutorService
 from app.infra.fastapi.dependables import get_core
+
+import aiofiles
+
+
+class GetStudentResponse(BaseModel):
+    first_name: str
+    last_name: str
+    balance: int
+    profile_address: str
+
+
+class ChangeFirstNameRequest(BaseModel):
+    new_first_name: str
+
+
+class ChangeLastNameRequest(BaseModel):
+    new_last_name: str
+
+
+class ChangePasswordRequest(BaseModel):
+    new_password: str
+
+
+class UploadProfilePictureRequest(BaseModel):
+    file: UploadFile = File(...)
+
 
 student_api = APIRouter()
 
 
 @student_api.get("/student/{student_mail}")
-def get_student(
-    student_mail: str,
-    core: OlympianTutorService = Depends(get_core),
+async def get_student(
+        student_mail: str,
+        core: OlympianTutorService = Depends(get_core),
 ):
+    print("/student/" + student_mail)
+
     student = core.get_student(student_mail)
-    core.change_student_last_name(student_mail, student.last_name)
-    core.change_student_first_name(student_mail, student.first_name)
-    pass
+    response = GetStudentResponse(
+        first_name=student.first_name,
+        last_name=student.last_name,
+        balance=student.balance,
+        profile_address=student.profile_address,
+    )
+
+    return response
+
+
+@student_api.post("/student/change_first_name/{student_mail}")
+async def change_first_name(
+        data: ChangeFirstNameRequest,
+        student_mail: str,
+        core: OlympianTutorService = Depends(get_core),
+):
+    print("/student/change_first_name/" + student_mail + " value: " + data.new_first_name)
+
+    student = core.get_student(student_mail)
+    core.change_student_first_name(student_mail, data.new_first_name)
+
+    return {"message": "Changed student first name successfully."}
+
+
+@student_api.post("/student/change_last_name/{student_mail}")
+async def change_last_name(
+        data: ChangeLastNameRequest,
+        student_mail: str,
+        core: OlympianTutorService = Depends(get_core),
+):
+    print("/student/change_last_name/" + student_mail + " value: " + data.new_last_name)
+
+    student = core.get_student(student_mail)
+    core.change_student_last_name(student_mail, data.new_last_name)
+
+    return {"message": "Changed student last name successfully."}
+
+
+@student_api.post("/student/change_password/{student_mail}")
+async def change_password(
+        data: ChangePasswordRequest,
+        student_mail: str,
+        core: OlympianTutorService = Depends(get_core),
+):
+    print("/student/change_password/" + student_mail + " value: " + data.new_password)
+
+    student = core.get_student(student_mail)
+    core.change_student_password(student_mail, data.new_password)
+
+    return {"message": "Changed student password successfully."}
+
+
+@student_api.post("/student/upload_profile_picture/{student_mail}")
+async def create_upload_file(
+        student_mail: str,
+        file: UploadFile = File(...),
+        core: OlympianTutorService = Depends(get_core),
+):
+    dest_path = '../../frontend/src/Storage/' + student_mail
+    async with aiofiles.open(dest_path, 'wb') as dest_file:
+        content = await file.read()
+        await dest_file.write(content)
+
+    core.change_student_profile_address(student_mail, dest_path)
--- frontend/src/Pages/Student.jsx
+++ frontend/src/Pages/Student.jsx
@@ -1,15 +1,168 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 
 export default function Student() {
   const location = useLocation();
   const email = location.state?.email;
 
+  const [data, setData] = useState([])
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const [balance, setBalance] = useState(0)
+  const [profileAddress, setProfileAddress] = useState('')
+
+  const [newFirstName, setNewFirstName] = useState('')
+  const [newLastName, setNewLastName] = useState('')
+  const [newPassword, setNewPassword] = useState('')
+
+  useEffect(() => {
+    const handleGetStudent = async () => {
+      try {
+        const response = await fetch('http://localhost:8000/student/' + email, {
+          method: 'GET',
+          headers: {
+          'Content-Type': 'application/json',
+          },
+        });
+        const data = await response.json();
+        setData(data)
+        console.log(data)
+
+        setFirstName(data['first_name'])
+        setLastName(data['last_name'])
+        setBalance(data['balance'])
+        setProfileAddress(data['profile_address'])
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    handleGetStudent();
+  }, []);
+
+  const handleChangeFirstName = async () => {
+    try {
+      const data = {
+        "new_first_name": newFirstName,
+      }
+
+      const response = await fetch('http://localhost:8000/student/change_first_name/' + email, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      });
+      response = await response.json();
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  const handleChangeLastName = async () => {
+    try {
+      const data = {
+        "new_last_name": newLastName,
+      }
+
+      const response = await fetch('http://localhost:8000/student/change_last_name/' + email, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      });
+      response = await response.json();
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  const handleChangePassword = async () => {
+    try {
+      const data = {
+        "new_password": newPassword,
+      }
+
+      const response = await fetch('http://localhost:8000/student/change_password/' + email, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      });
+      response = await response.json();
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const changeHandler = (event) => {
+    setSelectedImage(event.target.files[0]);
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    const form_data = new FormData();
+    form_data.append(
+      "file",
+      selectedImage,
+      selectedImage.name
+    );
+
+    const requestOptions = {
+      method: 'POST',
+      body: form_data
+    };
+    fetch('http://localhost:8000/student/upload_profile_picture/' + email, requestOptions)
+      .then(response => response.json())
+      .then(function (response) {
+        console.log(response)
+    });
+  }
+
   return (
     <div>
       <h1>Student Profile</h1>
+      <img src={require("../Storage/" + email)} style={{ width: 120, height: 140 }} />
+      <form onSubmit={handleSubmit}>
+        <input name="image" type="file" onChange={changeHandler} accept=".jpeg, .png, .jpg"/>
+        <button type="submit">Save</button>
+      </form>
       <p>Email: {email}</p>
       <p>Welcome to your student profile!</p>
+      <p>First Name: {firstName}</p>
+      <p>Last Name: {lastName}</p>
+      <p>Balance: {balance}</p>
+      <p>Profile Address: {profileAddress}</p>
+      <input
+        type="text"
+        value={newFirstName}
+        onChange={(e) => setNewFirstName(e.target.value)}
+        placeholder="new first name"
+      />
+      <button onClick={handleChangeFirstName}>Change First Name</button>
+      <br/>
+      <input
+        type="text"
+        value={newLastName}
+        onChange={(e) => setNewLastName(e.target.value)}
+        placeholder="new last name"
+      />
+      <button onClick={handleChangeLastName}>Change Last Name</button>
+      <br/>
+      <input
+        type="text"
+        value={newPassword}
+        onChange={(e) => setNewPassword(e.target.value)}
+        placeholder="new password"
+      />
+      <button onClick={handleChangePassword}>Change Password</button>
     </div>
   );
 }
