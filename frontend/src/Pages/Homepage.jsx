f8a4e269aa422d5ac172c1d3665aadad4a10e6e7
Shorena <sjanj19@freeuni.edu.ge>
2023-06-19 07:01:43 +0400

    Added a sidebar menu instead of bullet list of links

--- frontend/src/Pages/Homepage.jsx
+++ frontend/src/Pages/Homepage.jsx
@@ -1,37 +1,9 @@
 import * as React from "react"
-import { useState } from "react"
-import { Link } from "react-router-dom"
-import AppBar from "@mui/material/AppBar"
 import Box from "@mui/material/Box"
-import Toolbar from "@mui/material/Toolbar"
-import Button from "@mui/material/Button"
-import ModalDialog from "../Components/ModalDialog"
+import { Link } from "react-router-dom"
 
 export default function Homepage() {
-    const [registerOpen, setRegisterOpen] = useState(false);
-
-    const handleRegisterOpen = () => { setRegisterOpen(true); };
-    const handleRegisterClose = () => { setRegisterOpen(false); };
-
-    return (
-        <Box sx={{ flexGrow: 1 }}>
-            <ul>
-                <li><Link to="/">Homepage</Link></li>
-                <li><Link to="/registration">Registration</Link></li>
-                <li><Link to="/login">Login</Link></li>
-                <li><Link to="/about">About</Link></li>
-
-            </ul>
-            <AppBar position="static" style={{ background: '#CFCFCF' }}>
-                <Toolbar>
-                    <Button variant="text" sx={{ m: 0.5 }}>Homepage</Button>
-                    <Link to="/courses" sx={{ m: 0.5 }}>Find a Course</Link>
-                    <Link to="/tutors" sx={{ m: 0.5 }}>Find a Tutor</Link>
-                    <Button variant="contained" sx={{ m: 0.5 }} onClick={handleRegisterOpen}>Register</Button>
-                    <Button variant="contained" sx={{ m: 0.5 }}>Login</Button>
-                    <ModalDialog open={registerOpen} handleClose={handleRegisterClose} />
-                </Toolbar>
-            </AppBar>
-        </Box>
-    );
+  return (
+    <h1>Homepage</h1>
+  );
 }
