884cb7b7db87d3bfee97abd39306d8d154f2973e
Shorena <sjanj19@freeuni.edu.ge>
2023-06-19 07:55:00 +0400

    Student can buy a lesson from the courses' page

--- frontend/src/Pages/Courses.jsx
+++ frontend/src/Pages/Courses.jsx
@@ -1,10 +1,13 @@
 import { useState, useEffect } from 'react';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
+import IconButton from '@mui/material/IconButton';
+import AddBoxIcon from '@mui/icons-material/AddBox';
 import Course from "../Components/Course"
 
 export default function Courses() {
-  const email = localStorage.getItem("email")
+  const email = JSON.parse(localStorage.getItem("email"))
+  const isStudent = JSON.parse(localStorage.getItem("isStudent"))
   const [courses, setCourses] = useState([])
 
   useEffect(() => {
@@ -27,6 +30,28 @@ export default function Courses() {
     handleGetCourses();
   }, []);
 
+  const handleBuyLesson = async (course) => {
+    try {
+      const data = {
+        "subject": course.subject,
+        "tutor_mail": course.tutor_mail,
+        "lesson_price": course.price,
+      }
+
+      const response = await fetch('http://localhost:8000/student/buy_lesson/' + email, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      });
+      response = await response.json();
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
     <div>
       <h1>Courses</h1>
@@ -37,6 +62,9 @@ export default function Courses() {
               subject={course["subject"]}
               tutor_mail={course["tutor_mail"]}
               price={course["price"]}/>
+            <IconButton color="primary" onClick={(e) => handleBuyLesson(course)} disabled={!isStudent}>
+              <AddBoxIcon />
+            </IconButton>
           </ListItem>)}
       </List>
     </div>
