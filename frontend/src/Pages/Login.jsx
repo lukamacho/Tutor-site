629831b272fb7f5a3e5fb07b9f30c518f132a452
Shorena <sjanj19@freeuni.edu.ge>
2023-07-02 17:44:40 +0400

    Updated the Log In page

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -1,100 +1,51 @@
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
+import { styled } from '@mui/system';
 import Box from '@mui/material/Box';
+import Avatar from '@mui/material/Avatar';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import TextField from '@mui/material/TextField';
 import { useState } from 'react';
-import { useNavigate} from 'react-router-dom';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import backgroundImage from '../Images/LogInBG.png';
+import Grid from '@mui/material/Grid';
+import Link from '@mui/material/Link';
+import PropTypes from 'prop-types';
+import { useNavigate } from 'react-router-dom';
 
-function Copyright(props) {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Tutor site
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
+export const background = {
+  backgroundImage: `url(${backgroundImage})`,
+  backgroundSize: 'cover',
+  backgroundRepeat: 'repeat',
+  backgroundPosition: 'center',
+  minHeight: '100vh',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+};
+
+export const StyledBox = styled(Box)(({ theme }) => ({
+  marginTop: -100,
+  padding: 25,
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  borderRadius: '8px',
+  boxShadow: '0.5px 0.5px 10px rgba(0, 0, 0, 0.2)',
+  backgroundColor: 'rgba(255, 255, 255, 0.8)',
+  justifyContent: 'center',
+}));
+
+const centeredStyle = {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  marginBottom: 10,
 }
 
-const defaultTheme = createTheme();
-
-export default function SignIn() {
-  const navigate = useNavigate();
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [incorrectUser, setIncorrectUser] = useState(false)
-
-  const handleSubmit = async (e, is_student)=> {
-    e.preventDefault();
-
-    const data = {
-      "user_mail": email,
-      "password": password,
-      "is_student": is_student,
-    };
-
-    try {
-      const response = await fetch('http://localhost:8000/sign_in', {
-        method: 'POST',
-        body: JSON.stringify(data),
-        headers: { 'Content-Type': 'application/json' },
-      });
-      const result = await response.json();
-      console.log(result);
-
-      if (result.message !== "Email or password is incorrect.") {
-            console.log(result);
-
-        if (result.message === "Student signed in successfully."){
-
-            navigate('/student_profile', { state: { email } });
-        } else{
-            navigate('/tutor_profile', { state: { email } });
-        }
-      } else {
-        // Handle sign-in failure
-        setIncorrectUser(true)
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const handleResetPassword = async (e, is_student) => {
-    e.preventDefault();
-
-    const data = {
-      "user_mail": email,
-      "is_student": is_student,
-    };
-
-    try {
-      const response = await fetch('http://localhost:8000/admin/reset_password', {
-        method: 'POST',
-        body: JSON.stringify(data),
-        headers: { 'Content-Type': 'application/json' },
-      });
-      const result = await response.json();
-      console.log(result);
-
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const handleGoogleSignIn = async () => {
+const handleGoogleSignIn = async () => {
   try {
     const response = await fetch('http://localhost:8000/google_sign_in', {
       method: 'GET',
@@ -104,25 +55,95 @@ export default function SignIn() {
   } catch (error) {
     console.error(error);
   }
-};
+}
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="/">
+        Tutor site
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const LogIn = ({ setToken }) => {
+  const navigate = useNavigate();
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [authenticationSuccess, setAuthenticationSuccess] = useState(true);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const data = {
+      "email": email,
+      "password": password,
+    }
+
+    try {
+      const response = await fetch('http://localhost:8000/sign_in', {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: { 'Content-Type': 'application/json' },
+      });
+
+      const result = await response.json();
+      console.log(result);
+      setToken(result.token);
+
+      if (!result.error) {
+        sessionStorage.setItem('email', JSON.stringify(email));
+
+        if (result.is_student) {
+          // Student sign-in.
+          navigate('/student_profile');
+        } else {
+          // Tutor sign-in.
+          navigate('/tutor_profile');
+        }
+      } else {
+        setAuthenticationSuccess(false);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  const handleResetPassword = async (e) => {
+    e.preventDefault();
+
+    const data = {
+      "email": email,
+    }
+
+    try {
+      const response = await fetch('http://localhost:8000/admin/reset_password', {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: { 'Content-Type': 'application/json' },
+      });
+
+      const result = await response.json();
+      console.log(result);
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
   return (
-    <ThemeProvider theme={defaultTheme}>
+    <div style={background}>
       <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+        <StyledBox>
+          <Avatar sx={{ m: 1, bgcolor: 'customColors.darkPurple', }}>
             <LockOutlinedIcon />
           </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign in
+          <Typography component="h1" variant="h5" sx={{ m: 1.5 }}>
+            My Account
           </Typography>
           <Box  noValidate sx={{ mt: 1 }}>
             <TextField
@@ -150,51 +171,77 @@ export default function SignIn() {
               onChange={(e) => setPassword(e.target.value)}
             />
             <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
+              control={<Checkbox value="remember" sx={{ color: "customColors.darkPurple" }} />}
+              label="Remember Me"
             />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-              onClick={(e) => handleSubmit(e, true)}
+            <div style={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              marginTop: 20,
+              marginBottom: 0,
+              marginLeft: 0,
+              marginRight: 0, }}
             >
-               Sign In
-            </Button>
-            <div>
-                <Button
-                    onClick={(e) => handleResetPassword(e, true)}
-                    sx={{
-                        margin: 1,
-                    }}
-                    variant="outlined"
-                >
-                    Forgot password
-                </Button>
-                <Button
-                      fullWidth
-                      variant="contained"
-                      sx={{ mt: 3, mb: 2 }}
-                      onClick={handleGoogleSignIn}
-                >
-                  Sign In with Google
-                </Button>
+              <Button
+                fullWidth
+                type="submit"
+                variant="outlined"
+                sx={{ backgroundColor: 'customColors.peachPuff' }}
+                onClick={(e) => handleSubmit(e)}
+              >
+                Log In
+              </Button>
+              <Button
+                fullWidth
+                variant="outlined"
+                sx={{ backgroundColor: 'customColors.peachPuff' }}
+                onClick={handleGoogleSignIn}
+              >
+                Sign In with Google
+              </Button>
             </div>
-              <Grid item>
-                <Link href="/registration" variant="body2">
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
+            <Button
+              fullWidth
+              onClick={(e) => handleResetPassword(e)}
+              sx={{ mt: 3, mb: 2, mr: 0, ml: 0, }}
+              variant="outlined"
+            >
+              Forgot password
+            </Button>
+            <Link
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+                marginBottom: 10, }}
+              sx={{
+                color: 'customColors.darkPurple', }}
+              href="/registration" variant="body2"
+            >
+              {"Don't Have an Account Yet? Sign Up!"}
+            </Link>
+            {!authenticationSuccess && (
+              <Typography
+                style={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                  marginBottom: 10, }}
+                variant="body1"
+                color="error"
+              >
+                Email or password is incorrect.
+              </Typography>
+            )}
+            <Copyright/>
           </Box>
-        </Box>
-        {incorrectUser && (
-            <Typography variant="body1" color="error">
-              Email or password is incorrect.
-            </Typography>
-          )}
-        <Copyright sx={{ mt: 8, mb: 4 }} />
+        </StyledBox>
       </Container>
-    </ThemeProvider>
+    </div>
   );
 }
+
+LogIn.propTypes = {
+  setToken: PropTypes.func.isRequired,
+};
+
+export default LogIn;
