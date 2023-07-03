cd43669bfab988c7bf5f6fc482a209f555b7966e
Shorena <sjanj19@freeuni.edu.ge>
2023-07-03 20:23:32 +0400

    Added token authentication control on front side

---
+++ frontend/src/Components/TokenAuthentication.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+
+export const useToken = () => {
+  const getToken = () => {
+    const userToken = JSON.parse(sessionStorage.getItem('token'));
+    return userToken;
+  };
+
+  const [token, setToken] = useState(getToken());
+
+  const updateToken = userToken => {
+    sessionStorage.setItem('token', JSON.stringify(userToken));
+    setToken(userToken);
+  };
+
+  return {
+    token,
+    setToken: updateToken,
+  };
+}
