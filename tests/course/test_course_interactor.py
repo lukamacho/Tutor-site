0bec11e20e2a0bd25946a888989b031fde698b72
luka <lmach19@freeuni.edu.ge>
2023-07-09 20:14:32 +0400

    Course tests added

--- tests/course/test_course_interactor.py
+++ tests/course/test_course_interactor.py
@@ -63,7 +63,7 @@ def test_get_courses(course_repository: ICourseRepository) -> None:
 
     interactor.create_course(subject_1, tutor_mail, price_1)
     interactor.create_course(subject_2, tutor_mail, price_2)
-    get_response = interactor.get_courses(tutor_mail)
+    get_response = interactor.get_tutor_courses(tutor_mail)
 
     assert len(get_response) == num_subjects
     assert get_response[0].subject == subject_1
@@ -72,3 +72,23 @@ def test_get_courses(course_repository: ICourseRepository) -> None:
     assert get_response[1].tutor_mail == tutor_mail
     assert get_response[0].price == price_1
     assert get_response[1].price == price_2
+
+
+def test_delete_course(course_repository: ICourseRepository) -> None:
+    interactor = CourseInteractor(course_repository)
+
+    subject_1 = "Chemistry"
+    subject_2 = "Biology"
+    num_subjects = 2
+    tutor_mail = "tutor@gmail.com"
+    price_1 = 50
+    price_2 = 44
+
+    interactor.create_course(subject_1, tutor_mail, price_1)
+    interactor.create_course(subject_2, tutor_mail, price_2)
+    get_response = course_repository.get_tutor_courses(tutor_mail)
+
+    assert len(get_response) == num_subjects
+    interactor.delete_course(tutor_mail,subject_1)
+    get_response = course_repository.get_tutor_courses(tutor_mail)
+    assert len(get_response) == (num_subjects - 1)
