291fe87c92ca8a224e746baeb0b4c05d2b70bf1c
Shorena <sjanj19@freeuni.edu.ge>
2023-07-14 06:05:58 +0400

    Improved the student's profile page visually

--- frontend/src/Pages/Student.jsx
+++ frontend/src/Pages/Student.jsx
@@ -1,6 +1,82 @@
 import React, { useEffect, useState } from 'react';
 import { useLocation, Link, useNavigate } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import backgroundImage from '../Images/StudentProfileBG.png';
+import { HeaderStyledTypography } from "../Components/Styles"
+import Avatar from '@mui/material/Avatar';
+import { theme } from "../Components/Theme"
+import { ThemeProvider } from '@mui/material/styles';
+import { styled } from '@mui/system';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+
+const containerStyle = {
+  backgroundImage: `url(${backgroundImage})`,
+  backgroundSize: 'cover',
+  backgroundRepeat: 'repeat',
+  backgroundPosition: 'center',
+  minHeight: '100vh',
+};
+
+export const ProfileImage = styled(Box)(({ theme }) => ({
+  margin: 15,
+  width: 200,
+  height: 200,
+  display: 'flex',
+  alignItems: 'center',
+  borderRadius: '200px',
+  boxShadow: '0.5px 0.5px 10px rgba(0, 0, 0, 0.4)',
+  backgroundColor: 'rgba(255, 255, 255, 0.3)',
+  justifyContent: 'center',
+}));
+
+export const InfoBox = styled(Box)(({ theme }) => ({
+  margin: 15,
+  borderRadius: '10px',
+  boxShadow: '0.5px 0.5px 10px rgba(0, 0, 0, 0.4)',
+  backgroundColor: 'rgba(255, 255, 255, 0.8)',
+}));
+
+export const ListBox = styled(Box)(({ theme }) => ({
+  alignItems: 'center',
+  backgroundColor: 'rgba(255, 255, 255, 0)',
+  borderRadius: '14px',
+  display: 'flex',
+  flexDirection: 'row',
+  height: 400,
+  justifyContent: 'center',
+  margin: 'auto',
+  marginTop: 20,
+  width: "90%",
+  padding: 25,
+}));
+
+export const ProfileBox = styled(Box)(({ theme }) => ({
+  alignItems: 'center',
+  backgroundColor: 'rgba(248, 224, 255, 0)',
+  borderRadius: '14px',
+  display: 'flex',
+  justifyContent: 'center',
+  width: "100%",
+}));
+
+export const ListItemBox = styled(Box)(({ theme }) => ({
+  marginLeft: 20,
+  marginRight: 30,
+  width: '100%',
+  height: '100%',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  borderRadius: '10px',
+  boxShadow: '0.5px 0.5px 10px rgba(0, 0, 0, 0.4)',
+  backgroundColor: 'rgba(255, 255, 255, 0.5)',
+  justifyContent: 'center',
+}));
 
 const Student = ({ token }) => {
   let emailToken = JSON.parse(sessionStorage.getItem('email'))
@@ -27,10 +103,6 @@ const Student = ({ token }) => {
   const [report, setReport] = useState('');
 
   useEffect(() => {
-    localStorage.setItem("profileImage", JSON.stringify(profileAddress));
-  }, [email]);
-
-  useEffect(() => {
     const handleGetStudent = async () => {
       try {
         const response = await fetch('http://localhost:8000/student/' + email, {
@@ -46,6 +118,8 @@ const Student = ({ token }) => {
         setFirstName(data['first_name']);
         setLastName(data['last_name']);
         setBalance(data['balance']);
+
+        console.log("set profile address");
         setProfileAddress(data['profile_address']);
       } catch (error) {
         console.error(error);
@@ -54,6 +128,16 @@ const Student = ({ token }) => {
 
     handleGetStudent();
   }, []);
+
+  useEffect(() => {
+    const updateProfileAddress = async () => {
+      console.log("profile image session storage set item")
+      console.log(profileAddress)
+      sessionStorage.setItem("profileImage", JSON.stringify(profileAddress));
+    };
+
+    updateProfileAddress();
+  }, [profileAddress]);
 
   useEffect(() => {
     const handleGetLessons = async () => {
@@ -95,7 +179,7 @@ const Student = ({ token }) => {
     handleGetHomeworks();
   }, []);
 
- const handleNavigateToMessages = () => {
+  const handleNavigateToMessages = () => {
     navigate('/student/messages', { state: { email } });
   };
 
@@ -218,22 +302,22 @@ const Student = ({ token }) => {
     }
   };
 
-const handleFinishHomework = async (homework_text, tutor_mail) => {
-  const data = {
-    homework_text: homework_text,
-    tutor_mail: tutor_mail,
-    student_mail: email,
+  const handleFinishHomework = async (homework_text, tutor_mail) => {
+    const data = {
+      homework_text: homework_text,
+      tutor_mail: tutor_mail,
+      student_mail: email,
+    };
+    const response = await fetch('http://localhost:8000/student/finish_homework' , {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+    const response2 = await response.json();
+    console.log(response2);
   };
-  const response = await fetch('http://localhost:8000/student/finish_homework' , {
-    method: 'DELETE',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(data),
-  });
-  const response2 = await response.json();
-  console.log(response2);
-};
 
   const buyLesson = async (index) => {
     try {
@@ -258,83 +342,217 @@ const handleFinishHomework = async (homework_text, tutor_mail) => {
   };
 
   return (
-    <div>
-      <h1>Student Profile</h1>
-      <img
-        src={profileAddress === '' ? require('../Storage/default') : require('../Storage/' + email)}
-        style={{ width: 120, height: 140 }}
-      />
-      <form onSubmit={handleSubmit}>
-        <input name="image" type="file" onChange={changeHandler} accept=".jpeg, .png, .jpg" />
-        <button type="submit">Save</button>
-      </form>
-      <p>Email: {email}</p>
-      <p>Welcome to your student profile!</p>
-      <p>First Name: {firstName}</p>
-      <p>Last Name: {lastName}</p>
-      <p>Balance: {balance}</p>
-      <p>Profile Address: {profileAddress}</p>
-      <input
-        type="text"
-        value={newFirstName}
-        onChange={(e) => setNewFirstName(e.target.value)}
-        placeholder="new first name"
-      />
-      <button onClick={handleChangeFirstName}>Change First Name</button>
-      <br />
-      <input
-        type="text"
-        value={newLastName}
-        onChange={(e) => setNewLastName(e.target.value)}
-        placeholder="new last name"
-      />
-      <button onClick={handleChangeLastName}>Change Last Name</button>
-      <br />
-      <input
-        type="text"
-        value={newPassword}
-        onChange={(e) => setNewPassword(e.target.value)}
-        placeholder="new password"
-      />
-      <button onClick={handleChangePassword}>Change Password</button>
-      <div>
-        <button onClick={handleNavigateToMessages}>Go to Messages</button>
-      </div>
-      <h1>Lessons:</h1>
-      <ul>
-        {lessons.map((lesson, index) => (
-          <li key={index}>
-            Subject: {lesson['subject']}; Tutor:{' '}
-            <Link to={'http://localhost:3000/tutors/tutor/' + lesson['tutor_mail']}>{lesson['tutor_mail']}</Link>
-            Number of lessons: {lesson['number_of_lessons']}; Lesson price: {lesson['lesson_price']};
-            <button onClick={(e) => buyLesson(index)}>Buy Lesson</button>
-          </li>
-        ))}
-      </ul>
-      <h4>Contact Admin</h4>
-      <input
-        type="text"
-        value={report}
-        onChange={(e) => setReport(e.target.value)}
-        placeholder="my report"
-      />
-      <button onClick={handleSendReportToAdmin}>Send to Admin</button>
-      <input type="number" value={newBalance} onChange={(e) => setNewBalance(e.target.value)} />
-      <button onClick={handleAddBalance}>Add Balance</button>
-
-      <h1>Homeworks:</h1>
-      {homeworks.length < 1 ? (
-        <p>Woohoo! No homework.</p>
-      ) : (
-       <ul>
-          {homeworks.map((homework, index) => (
-            <li key={index}>
-              {homework.homework_text}
-              <button onClick={() => handleFinishHomework(homework.homework_text,homework.tutor_mail)}>Finish</button>
-            </li>
-          ))}
-        </ul>
-      )}
+    <div style={containerStyle}>
+      <ThemeProvider theme={theme}>
+        <HeaderStyledTypography variant="h4" align="center">
+          My Profile
+        </HeaderStyledTypography>
+        <div style={{ display: 'flex', padding: 10 }}>
+          <ProfileBox>
+            <ProfileImage>
+              <Avatar
+                style={{ width: 180, height: 180 }}
+                src={profileAddress === '' ? require('../Storage/default') : require('../Storage/' + email)} />
+            </ProfileImage>
+            <InfoBox>
+              <List>
+                <ListItem style={{ margin: -5 }}>
+                  <Typography variant="h6" sx={{ fontWeight: 'bold' }}>
+                    Email:
+                  </Typography>
+                  <Typography variant="h6" sx={{ marginLeft: 1, }}>
+                    {email}
+                  </Typography>
+                </ListItem>
+                <ListItem style={{ margin: -5 }}>
+                  <Typography variant="h6" sx={{ fontWeight: 'bold' }}>
+                    First Name:
+                  </Typography>
+                  <Typography variant="h6" sx={{ marginLeft: 1, }}>
+                    {firstName}
+                  </Typography>
+                </ListItem>
+                <ListItem style={{ margin: -5 }}>
+                  <Typography variant="h6" sx={{ fontWeight: 'bold' }}>
+                    Last Name:
+                  </Typography>
+                  <Typography variant="h6" sx={{ marginLeft: 1, }}>
+                    {lastName}
+                  </Typography>
+                </ListItem>
+                <ListItem style={{ margin: -5 }}>
+                  <Typography variant="h6" sx={{ fontWeight: 'bold' }}>
+                    Balance:
+                  </Typography>
+                  <Typography variant="h6" sx={{ marginLeft: 1, }}>
+                    {balance}
+                  </Typography>
+                </ListItem>
+              </List>
+            </InfoBox>
+            <InfoBox>
+              <List>
+                <ListItem>
+                  <TextField
+                    id="new-first-name"
+                    fullWidth
+                    label="New First Name"
+                    name="new-first-name"
+                    onChange={e => setNewFirstName(e.target.value)}
+                    value={newFirstName}
+                    size="small"
+                  />
+                  <Button
+                    onClick={handleChangeFirstName}
+                    sx={{ marginLeft: 1, }}
+                    variant="outlined"
+                    size="medium"
+                  >
+                    Change
+                  </Button>
+                </ListItem>
+                <ListItem>
+                  <TextField
+                    id="new-last-name"
+                    fullWidth
+                    label="New Last Name"
+                    name="new-last-name"
+                    onChange={e => setNewLastName(e.target.value)}
+                    value={newLastName}
+                    size="small"
+                  />
+                  <Button
+                    onClick={handleChangeLastName}
+                    sx={{ marginLeft: 1, }}
+                    variant="outlined"
+                    size="medium"
+                  >
+                    Change
+                  </Button>
+                </ListItem>
+                <ListItem>
+                  <TextField
+                    id="new-password"
+                    fullWidth
+                    label="New Password"
+                    name="new-password"
+                    onChange={e => setNewPassword(e.target.value)}
+                    value={newPassword}
+                    size="small"
+                  />
+                  <Button
+                    onClick={handleChangePassword}
+                    sx={{ marginLet: 1, }}
+                    variant="outlined"
+                    size="medium"
+                  >
+                    Change
+                  </Button>
+                </ListItem>
+                <ListItem>
+                  <input name="image" type="file" onChange={changeHandler} accept=".jpeg, .png, .jpg" />
+                  <Button
+                    onClick={handleSubmit}
+                    sx={{ marginLet: 1, }}
+                    variant="outlined"
+                    size="medium"
+                  >
+                    Save
+                  </Button>
+                </ListItem>
+              </List>
+            </InfoBox>
+            <InfoBox>
+              <List>
+                <ListItem>
+                  <Button
+                    onClick={handleNavigateToMessages}
+                    sx={{ marginLet: 1, }}
+                    variant="outlined"
+                    size="medium"
+                  >
+                    Messages
+                  </Button>
+                </ListItem>
+                <ListItem>
+                  <TextField
+                    label="My Report"
+                    onChange={e => setReport(e.target.value)}
+                    value={report}
+                    size="small"
+                  />
+                  <Button
+                    onClick={handleSendReportToAdmin}
+                    sx={{ marginLet: 1, }}
+                    variant="outlined"
+                    size="medium"
+                  >
+                    Send to Admin
+                  </Button>
+                </ListItem>
+                <ListItem>
+                  <TextField
+                    label="New Balance"
+                    onChange={e => setNewBalance(e.target.value)}
+                    value={newBalance}
+                    size="small"
+                  />
+                  <Button
+                    onClick={handleAddBalance}
+                    sx={{ marginLet: 1, }}
+                    variant="outlined"
+                    size="medium"
+                  >
+                    Add Balance
+                  </Button>
+                </ListItem>
+              </List>
+            </InfoBox>
+          </ProfileBox>
+        </div>
+        <ListBox>
+          <ListItemBox>
+            <Typography variant="h4" sx={{ fontWeight: 'bold' }}>
+              Lessons:
+            </Typography>
+            {lessons.length < 1 ? (
+              <Typography variant="h5" sx={{ marginLeft: 1, }}>
+                No lessons!
+              </Typography>
+            ) : (
+              <List>
+                {lessons.map((lesson, index) => (
+                  <ListItem key={index}>
+                    Subject: {lesson['subject']}; Tutor:{' '}
+                    <Link to={'http://localhost:3000/tutors/tutor/' + lesson['tutor_mail']}>{lesson['tutor_mail']}</Link>
+                    Number of lessons: {lesson['number_of_lessons']}; Lesson price: {lesson['lesson_price']};
+                    <Button onClick={(e) => buyLesson(index)}>Buy Lesson</Button>
+                  </ListItem>
+                ))}
+              </List>
+            )}
+          </ListItemBox>
+          <ListItemBox>
+            <Typography variant="h4" sx={{ fontWeight: 'bold' }}>
+              Homeworks:
+            </Typography>
+            {homeworks.length < 1 ? (
+              <Typography variant="h5" sx={{ marginLeft: 1, }}>
+                No homework!
+              </Typography>
+            ) : (
+              <List>
+                {homeworks.map((homework, index) => (
+                  <ListItem key={index}>
+                    {homework.homework_text}
+                    <button onClick={() => handleFinishHomework(homework.homework_text,homework.tutor_mail)}>Finish</button>
+                  </ListItem>
+                ))}
+              </List>
+            )}
+          </ListItemBox>
+        </ListBox>
+      </ThemeProvider>
     </div>
   );
 }
