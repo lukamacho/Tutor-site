a8560e7c7cb03f6882ff2837dea8e22e7c60c251
Shorena <sjanj19@freeuni.edu.ge>
2023-07-17 11:33:05 +0400

    Beautified student messages' page

--- frontend/src/Pages/StudentMessages.jsx
+++ frontend/src/Pages/StudentMessages.jsx
@@ -1,5 +1,38 @@
 import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
+import backgroundImage from '../Images/MessagesBG.png';
+import { theme } from "../Components/Theme"
+import { ThemeProvider } from '@mui/material/styles';
+import { HeaderStyledTypography } from "../Components/Styles"
+import { styled } from '@mui/system';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+
+const containerStyle = {
+  backgroundImage: `url(${backgroundImage})`,
+  backgroundSize: 'cover',
+  backgroundRepeat: 'repeat',
+  backgroundPosition: 'center',
+  minHeight: '100vh',
+};
+
+export const ListBox = styled(Box)(({ theme }) => ({
+  alignItems: 'center',
+  backgroundColor: 'rgba(255, 255, 255, 0.5)',
+  borderRadius: '14px',
+  display: 'flex',
+  flexDirection: 'row',
+  height: 'auto',
+  justifyContent: 'center',
+  margin: 'auto',
+  marginTop: 20,
+  width: "90%",
+  padding: 25,
+}));
 
 export default function StudentMessages() {
   const location = useLocation();
@@ -81,43 +114,78 @@ export default function StudentMessages() {
   };
 
   return (
-    <div>
-      <h1>Student Messages</h1>
-      <p>Email: {email}</p>
-      <h2>Messaged Tutors:</h2>
-      {messagedTutors.length === 0 ? (
-        <p>No messages</p>
-      ) : (
-        <ul>
-          {messagedTutors.map((tutor, index) => (
-            <li key={index} onClick={() => handleTutorClick(tutor)}>
-              {tutor}
-            </li>
-          ))}
-        </ul>
-      )}
-      {receiverMail && <p>Receiver Mail: {receiverMail}</p>}
-      <div>
-        <input
-          type="text"
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-          placeholder="Enter your message"
-        />
-        <button onClick={handleSendMessage}>Send Message</button>
-      </div>
-      <div>
-        <h2>Messages:</h2>
-        {messages.length === 0 ? (
-          <p>No messages</p>
-        ) : (
-          <ul>
-            {messages.map((message, index) => (
-              <li key={index}>{message.message_text}</li>
-            ))}
-          </ul>
-        )}
-      </div>
+    <div style={containerStyle}>
+      <ThemeProvider theme={theme}>
+        <HeaderStyledTypography variant="h4" align="center">
+          Student's Messages
+        </HeaderStyledTypography>
+        <ListBox>
+          <Typography variant="h4" sx={{ fontWeight: 'bold' }}>
+            Messaged Tutors:
+          </Typography>
+          {messagedTutors.length === 0 ? (
+            <Typography variant="h5" sx={{ marginLeft: 1, }}>
+              No Messages!
+            </Typography>
+          ) : (
+            <List>
+              {messagedTutors.map((tutor, index) => (
+                <ListItem key={index} onClick={() => handleTutorClick(tutor)}>
+                  {tutor}
+                </ListItem>
+              ))}
+            </List>
+          )}
+        </ListBox>
+        <ListBox>
+          {receiverMail &&
+            <div>
+              <Typography variant="h6" sx={{ fontWeight: 'bold' }}>
+                Receiver Mail:
+              </Typography>
+              <Typography variant="h6" sx={{ marginLeft: 1, }}>
+                {receiverMail}
+              </Typography>
+            </div>}
+          <TextField
+            id="message"
+            fullWidth
+            label="Enter Your Message"
+            name="message"
+            onChange={e => setMessage(e.target.value)}
+            value={message}
+            size="small"
+          />
+          <Button
+            onClick={handleSendMessage}
+            sx={{ marginLeft: 1, }}
+            variant="outlined"
+            size="small"
+          >
+            Send
+          </Button>
+        </ListBox>
+        <ListBox>
+          <Typography variant="h4" sx={{ fontWeight: 'bold' }}>
+            Messages:
+          </Typography>
+          {messages.length === 0 ? (
+            <Typography variant="h5" sx={{ marginLeft: 1, }}>
+              No Messages!
+            </Typography>
+          ) : (
+            <List>
+              {messages.map((message, index) => (
+                <ListItem key={index}>
+                  <Typography variant="h6" sx={{ marginLeft: 1, }}>
+                    {message.message_text}
+                  </Typography>
+                </ListItem>
+              ))}
+            </List>
+          )}
+        </ListBox>
+      </ThemeProvider>
     </div>
   );
 }
